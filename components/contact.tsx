beb8fc6cdb435183569cbd9fdfd748dcb3f2bcf2
Raman Nagar <115226318+Raman-Nagar@users.noreply.github.com>
2024-03-03 10:55:38 +0530

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="ramannagar08082000@gmail.com">
+          ramannagar08082000@gmail.com
         </a>{" "}
         or through this form.
       </p>
@@ -49,7 +49,8 @@ export default function Contact() {
             return;
           }
 
-          toast.success("Email sent successfully!");
+          // toast.success("Email sent successfully!");
+          toast.success("Thanks for contacting us! We will be in touch with you shortly.");
         }}
       >
         <input
